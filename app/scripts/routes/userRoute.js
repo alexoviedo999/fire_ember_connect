0f905d7e5cb66318ea5177a8d2e3ab4bdfc24e63
alexoviedo999 <alexoviedo999@gmail.com>
2014-04-14 23:48:17 -0400

    fix routes

--- app/scripts/routes/userRoute.js
+++ app/scripts/routes/userRoute.js
@@ -1,5 +1,12 @@
-App.UserRoute = Ember.Route.extend({
+FireEmberConnect.UserRoute = Ember.Route.extend({
   model: function(params) {
     return this.store.find('user', params.user_id);
+  }
+});
+
+FireEmberConnect.UserEditRoute=Em.Route.extend({
+  afterModel:function(){
+    // return this.modelFor('user');
+    this.set('user', this.modelFor('user'));
   }
 });
